454d55713458247000309da23075c2e8c823daed
Patricio Bruna <pbruna@gmail.com>
2015-03-03 19:41:32 -0300

    Primera version para probar

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-(function() {
-
-  return {
-    events: {
-      'app.activated':'doSomething'
-    },
-
-    doSomething: function() {
-    }
-  };
-
+(function() {	
+	return {
+		requests: {
+			fetchContactInfo: function(email, token) {
+				return {
+					url: 'https://api.pipedrive.com/v1/persons/find?search_by_email=1&term='+email+'&api_token='+token,
+					type: 'GET',
+					dataType: 'json'
+				};
+			}	
+		},
+		
+		events: {
+			'app.activated':'doSomething'
+		},
+		
+		doSomething: function() {
+			var requester_email = this.ticket().requester().email();
+			this.ajax('fetchContactInfo', requester_email , this.setting("pipedrive_token") )
+			.done(function(data){
+				var contact = data.data[0];
+				this.switchTo('contact', contact);
+			});
+		}
+	};
 }());
